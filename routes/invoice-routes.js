feccd990309203935117c468acdf2adeeab2a960
James Hagood <cardfaux@tutanota.com>
2020-03-05 14:33:56 -0600

    added file upload on backend

--- routes/invoice-routes.js
+++ routes/invoice-routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const invoiceControllers = require('../controllers/invoice-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
 //@ Route /api/invoice
 //@ Method POST
-router.post('/', invoiceControllers.invoiceImage);
+router.post('/', fileUpload.single('image'), invoiceControllers.invoiceImage);
 
 module.exports = router;
