bd77153f8420fae46f11f6a3a9d3e375e58cb347
James Hagood <cardfaux@tutanota.com>
2020-03-05 13:20:19 -0600

    added error handling and middleware

--- app.js
+++ app.js
@@ -1,10 +1,48 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 
 const invoiceRoutes = require('./routes/invoice-routes');
 
 const app = express();
 
+app.use(express.json());
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+
+// CORS Middleware to attatch to every response
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+	next();
+});
+// CORS Middleware to attatch to every response
+
 app.use('/api/invoice', invoiceRoutes);
+
+// Error Handling Routes And MiddleWare
+app.use((req, res, next) => {
+	const error = new HttpError('Could Not Find This Route', 404);
+	throw error;
+});
+
+app.use((error, req, res, next) => {
+	// Rollback File Upload If We Get An Error
+	if (req.file) {
+		fs.unlink(req.file.path, (err) => {
+			console.log(err);
+		});
+	}
+	if (res.headersSent) {
+		return next(error);
+	}
+	res.status(error.code || 500);
+	res.json({ message: error.message || 'An Unknown Error Occurred' });
+});
+// Error Handling Routes And MiddleWare
 
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => `I Am Running On Port ${PORT}`);
