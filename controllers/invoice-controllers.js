9fb9e732170654d10069254264e892e360e56e8f
James Hagood <cardfaux@tutanota.com>
2020-03-06 15:00:47 -0600

    added regex to grab invoice number starting with #

--- controllers/invoice-controllers.js
+++ controllers/invoice-controllers.js
@@ -16,16 +16,37 @@ const invoiceImage = (req, res, next) => {
 		fs.readFile(`./uploads/images/${req.file.filename}`, (err, data) => {
 			if (err) return console.log('This Is Your Error', err);
 
-			(async () => {
-				await worker.load();
-				await worker.loadLanguage('eng');
-				await worker.initialize('eng');
-				const {
-					data: { text }
-				} = await worker.recognize(data);
-				console.log(text);
-				await worker.terminate();
-			})();
+			let loadedImage = async () => {
+				try {
+					await worker.load();
+					await worker.loadLanguage('eng');
+					await worker.initialize('eng');
+					const {
+						data: { text }
+					} = await worker.recognize(data);
+					// console.log(text);
+					// console.log(typeof text);
+					// let split = text.split(' ');
+					// console.log(split);
+					let invoiceRegex = /#[\d]+/gi;
+					let invoiceNum = invoiceRegex.exec(text);
+					let invoiceNumber = invoiceNum[0];
+					console.log(invoiceNumber);
+					await worker.terminate();
+				} catch (error) {
+					console.log(error);
+				}
+			};
+			loadedImage();
+
+			//loadedImage();
+
+			// let stringImage = new String(loadedImage());
+			// console.log(stringImage);
+
+			// let stringImage = loadedImage.toString();
+			// let splitText = stringImage.split(' ');
+			// console.log(splitText);
 		});
 	}
 };
