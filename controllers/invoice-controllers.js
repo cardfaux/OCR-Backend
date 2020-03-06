9eb5460572544b1fcfc9aab78ab0de3533148473
James Hagood <cardfaux@tutanota.com>
2020-03-05 19:43:47 -0600

    set up tesseract on an incoming image

--- controllers/invoice-controllers.js
+++ controllers/invoice-controllers.js
@@ -1,10 +1,33 @@
 const HttpError = require('../models/http-error');
+const fs = require('fs');
+
+const { createWorker } = require('tesseract.js');
+
+const worker = createWorker({
+	logger: (m) => console.log(m) // Add logger here
+});
 
 //@ Route /api/invoice
 //@ Method POST
 const invoiceImage = (req, res, next) => {
-	console.log('invoiceImageController');
-	res.json({ message: 'it works' });
+	console.log(req.file);
+
+	if (req.file) {
+		fs.readFile(`./uploads/images/${req.file.filename}`, (err, data) => {
+			if (err) return console.log('This Is Your Error', err);
+
+			(async () => {
+				await worker.load();
+				await worker.loadLanguage('eng');
+				await worker.initialize('eng');
+				const {
+					data: { text }
+				} = await worker.recognize(data);
+				console.log(text);
+				await worker.terminate();
+			})();
+		});
+	}
 };
 
 exports.invoiceImage = invoiceImage;
